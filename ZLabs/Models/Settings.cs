fd9b783cd7f1ee47e3505e8c2772be0f70bd2c60
hheimerd <32-kot@mail.ru>
2022-03-21 15:17:23 +0300

    add settings page

---
+++ ZLabs/Models/Settings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ZLabs.ViewModels;
+
+namespace ZLabs.Models;
+
+public static class Settings
+{
+    public static double ExperimentTime { get; set; } = 1;
+    public static TimeUnit TimeUnit { get; set; } = TimeUnit.TimeUnits[0];
+    public static TimeFormat TimeFormat { get; set; } = TimeFormat.Stopwatch;
+    public static ChartFormat ChartFormat { get; set; } = ChartFormat.Dots | ChartFormat.Line;
+}
+
+[Flags]
+public enum ChartFormat
+{
+    Line,
+    Dots
+}
+
+public enum TimeFormat
+{
+    Stopwatch,
+    MinutesSeconds,
+    HoursMinutes
+}
+
+public class TimeUnit
+{
+    public string Label { get; }
+    public double Multiplier{ get; }
+    
+    public static TimeUnit[]  TimeUnits = new TimeUnit[]
+    {
+        new("Секунда", 1),
+        new("Минута", 1 / 60d),
+        new("Час", 1 / 60d / 60d),
+    };
+    
+    public TimeUnit(string label, double multiplier)
+    {
+        Label = label;
+        Multiplier = multiplier;
+    }
+    
+    public override string ToString()
+    {
+        return Label;
+    }
+}
---
+++ ZLabs/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using ZLabs.Models;
+
+namespace ZLabs.ViewModels;
+
+public class SettingsViewModel : IPage
+{
+    string IPage.Name => "Настройки";
+    string IPage.ImagePath => "/Assets/Img/Gear.png";
+
+    public ChartFormatListBoxItem SelectedChartFormatItem
+    {
+        get => ChartFormats.First(item => item.Format == Settings.ChartFormat);
+        set => Settings.ChartFormat = value.Format;
+    }
+
+    public TimeFormatListBoxItem SelectedTimeFormatItem
+    {
+        get => TimeFormats.First(item => item.Format == Settings.TimeFormat);
+        set => Settings.TimeFormat = value.Format;
+    }
+
+    public TimeUnit SelectedTimeUnit
+    {
+        get => Settings.TimeUnit;
+        set => Settings.TimeUnit = value;
+    }
+    
+    public double ExperimentTime
+    {
+        get => Settings.ExperimentTime;
+        set => Settings.ExperimentTime = value;
+    }
+
+    public TimeFormatListBoxItem[] TimeFormats { get; }
+    public ChartFormatListBoxItem[] ChartFormats { get; }
+    public TimeUnit[] TimeUnits => TimeUnit.TimeUnits;
+
+    public SettingsViewModel()
+    {
+        TimeFormats = new TimeFormatListBoxItem[]
+        {
+            new("Секундомер", TimeFormat.Stopwatch),
+            new("ММ:СС", TimeFormat.MinutesSeconds),
+            new("ЧЧ:ММ", TimeFormat.HoursMinutes),
+        };
+
+        ChartFormats = new ChartFormatListBoxItem[]
+        {
+            new("Линия и точки", ChartFormat.Line | ChartFormat.Dots),
+            new("Линия", ChartFormat.Line),
+            new("Точки", ChartFormat.Dots),
+        };
+    }
+}
+
+public class TimeFormatListBoxItem
+{
+    public string Label;
+    public TimeFormat Format;
+
+    public TimeFormatListBoxItem(string label, TimeFormat format)
+    {
+        Label = label;
+        Format = format;
+    }
+
+    public override string ToString()
+    {
+        return Label;
+    }
+}
+
+public class ChartFormatListBoxItem
+{
+    public string Label;
+    public ChartFormat Format;
+
+    public ChartFormatListBoxItem(string label, ChartFormat format)
+    {
+        Label = label;
+        Format = format;
+    }
+
+    public override string ToString()
+    {
+        return Label;
+    }
+}
